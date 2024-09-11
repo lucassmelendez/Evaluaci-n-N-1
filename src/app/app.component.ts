68e0bf4f5de805c7dcdbf348fcef9b953d5fe76d
lucassmelendez <luc.melendez@duocuc.cl>
2024-09-10 21:23:42 -0300

    funcionalidad del menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController, ModalController, NavController } from '@ionic/angular';
-
+import { MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,9 @@ export class AppComponent {
 
   constructor(private modalController: ModalController,
     public alertController: AlertController,
-    public navCtrl: NavController,) {
+    public navCtrl: NavController,
+    private menu: MenuController,
+    private router: Router) {
   }
 
   async salir(){
@@ -30,17 +33,29 @@ export class AppComponent {
           text: 'Si',
           handler: () =>{
             this.navCtrl.navigateRoot('login');
+            this.menu.close()
           }
         }
       ]
     });
 
     await alert.present();
-  
+
   }
 
   goBack() {
-    this.navCtrl.back();
-  }
+    if (this.router.url == '/home') {
+      this.menu.close()
+    }else{
+      this.navCtrl.back();
+      this.menu.close();
+  }                                                                                                                                                 
+}                                                                                                                                                 
 
+  navigateAndClose(route: string) {
+    this.menu.close('main').then(() => {
+      this.router.navigate([route]);
+      this.menu.close()
+    });
+  }
 }

1af16bd8d26674ec498e302fe36af8b570e28143
Matías Recabarren <mat.recabarren@duocuc.cl>
2024-10-26 23:16:54 -0300

    Integracion BD de DJango
    Grabación 2-10
    minuto 30:16

--- RegistrAppDJ/API/models.py
+++ RegistrAppDJ/API/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator, MinLengthValidator 
 
 # Create your models here.
+class Usuario(models.Model):
+    id=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=45,null=False)
+    apellido=models.CharField(max_length=50,default='S/A')
+    edad=models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.nombre
+    
+class alumno(models.Model):
+    apellido=models.CharField(max_length=50,default='S/A')
+    correo=models.CharField(max_length=30,null=False,validators=[EmailValidator(message="Ingrese un correo válido.")] )
+    edad=models.IntegerField()
+    nombre=models.CharField(max_length=50,null=False)
+    password=models.CharField(max_length=20,null=False)
+    password2=models.CharField(max_length=20,null=False)
+
+    def __str__(self) -> str:
+        return self.nombre+' '+self.apellido
+    
+    # Validación adicional para contraseñas iguales
+    def clean(self):
+        super().clean()
+        if self.password != self.password2:
+            raise ValidationError("Las contraseñas no coinciden.")

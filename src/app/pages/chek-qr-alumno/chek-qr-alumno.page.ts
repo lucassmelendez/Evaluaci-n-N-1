2675dcb41ee5e2712b3873d5d2b1ff462817050b
lucassmelendez <luc.melendez@duocuc.cl>
2024-10-27 14:58:25 -0300

    qr listo

--- src/app/pages/chek-qr-alumno/chek-qr-alumno.page.ts
+++ src/app/pages/chek-qr-alumno/chek-qr-alumno.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-chek-qr-alumno',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chek-qr-alumno.page.scss'],
 })
 export class ChekQRAlumnoPage implements OnInit {
+  
+  isSupported = false;
+  barcodes: Barcode[] = [];
 
-  constructor() { }
+  constructor(private AlertController:AlertController) { }
 
   ngOnInit() {
+    BarcodeScanner.isSupported().then((result) => {
+      this.isSupported = result.supported;
+    });
+  }
+  
+  async scan(): Promise<void> {
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
   }
 
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async presentAlert(): Promise<void> {
+    const alert = await this.AlertController.create({
+      header: 'Permiso Denegado',
+      message: 'Por favor establezca permisos para el uso de barcode scanner.',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 }
